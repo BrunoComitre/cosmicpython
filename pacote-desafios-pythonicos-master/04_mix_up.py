6fe3fcab7d579052e8153e5e125c04581abe499e
Bruno Alves Comitre <bruno-comitre@hotmail.com>
2021-11-02 14:36:14 -0300

    V2

--- pacote-desafios-pythonicos-master/04_mix_up.py
+++ pacote-desafios-pythonicos-master/04_mix_up.py
@@ -1,4 +1,4 @@
-# V1
+# V2
 
 """
 04. mix_up
@@ -14,7 +14,7 @@ Exemplo:
 Assuma que a e b tem tamanho 2 ou maior.
 """
 
-def mix_up(a, b):
+def mix_up(a: str, b: str) -> str:
     # +++ SUA SOLUÇÃO +++
     return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
